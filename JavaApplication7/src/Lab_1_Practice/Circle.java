713c7cac2be4177d702cde8ec6bcfb7323d0e5ab
nahmad0315 <101325089+nahmad0315@users.noreply.github.com>
2023-09-15 09:58:16 +0500

    Add files via upload
    This is example of abstraction.

---
+++ JavaApplication7/src/Lab_1_Practice/Circle.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab_1_Practice;
+
+/**
+ *
+ * @author Nabeel Ahmad
+ */
+public class Circle extends Shape {
+    private double radius;
+    
+    public Circle(double radius){
+        this.radius = radius;
+    }
+    @Override
+    public double calculateArea(){
+        return Math.PI * radius * radius;
+    }
+    
+    public double calculatePerimeter() {
+        System.out.println("This method calculates the peri of the circle " + 2 * Math.PI * radius );
+        return 2 * Math.PI * radius;
+    }
+    
+    public void displayInfo(){
+        System.out.println("This is a circle");
+    }
+}
