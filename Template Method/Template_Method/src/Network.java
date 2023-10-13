176a9b9afd412929711093f8c8a8dfc7984d6c9f
nahmad0315 <101325089+nahmad0315@users.noreply.github.com>
2023-10-13 10:15:24 +0500

    Template Method
    Template Method Im[plemented...

---
+++ Template Method/Template_Method/src/Network.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author FA20-BSE-062
+ */
+public abstract class Network {
+    String userName;
+    String password;
+
+    Network() {}
+
+    /**
+     * Publish the data to whatever network.
+     */
+    public boolean post(String message) {
+        // Authenticate before posting. Every network uses a different
+        // authentication method.
+        if (logIn(this.userName, this.password)) {
+            // Send the post data.
+            boolean result =  sendData(message.getBytes());
+            logOut();
+            return result;
+        }
+        return false;
+    }
+
+    abstract boolean logIn(String userName, String password);
+    abstract boolean sendData(byte[] data);
+    abstract void logOut();
+}
