dbff00e3cc5c621d2a69be115eb97a23e9e6c564
nahmad0315 <101325089+nahmad0315@users.noreply.github.com>
2023-10-13 09:32:59 +0500

    Iterator Pattern
    Iterator Pattern is Implemented

---
+++ Iterator/src/Exapmle_1/NameRepository.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exapmle_1;
+
+/**
+ *
+ * @author Nabeel Ahmad
+ */
+public class NameRepository implements Container {
+   public String names[] = {"Robert" , "John" ,"Julie" , "Lora"};
+
+   @Override
+   public Iterator getIterator() {
+      return new NameIterator();
+   }
+
+   private class NameIterator implements Iterator {
+
+      int index;
+
+      @Override
+      public boolean hasNext() {
+      
+         if(index < names.length){
+            return true;
+         }
+         return false;
+      }
+
+      @Override
+      public Object next() {
+      
+         if(this.hasNext()){
+            return names[index++];
+         }
+         return null;
+      }		
+   }
+}
