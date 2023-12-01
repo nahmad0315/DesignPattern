c9e98e8b853841a4258df57c03e379085b10fd8d
nahmad0315 <101325089+nahmad0315@users.noreply.github.com>
2023-12-01 09:53:28 +0500

    new tasks added

---
+++ Gang_of_Four/AbstractFactory/src/TP_Example/ShapeFactory.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TP_Example;
+
+/**
+ *
+ * @author shafq
+ */
+public class ShapeFactory extends AbstractFactory {
+   @Override
+   public Shape getShape(String shapeType){    
+      if(shapeType.equalsIgnoreCase("RECTANGLE")){
+         return new Rectangle();         
+      }else if(shapeType.equalsIgnoreCase("SQUARE")){
+         return new Square();
+      }	 
+      return null;
+   }
+}
